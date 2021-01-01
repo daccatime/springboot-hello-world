1e5fef7b94cfd42ea3f7ae855c360673c5ff8c19
daccatime <dawsonmcnally@outlook.com>
2021-01-01 14:42:17 +0000

    first microservice used

--- src/main/java/com/example/helloworld/HelloWorldController.java
+++ src/main/java/com/example/helloworld/HelloWorldController.java
@@ -7,16 +7,16 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HelloWorldController {
-
+// this works
 	@RequestMapping(value = { "/hello" }, method = RequestMethod.GET)
 	public String sayHelloWorld() {
-		String responseMsg = new StringBuilder("<h1>").append("Hey...Hello World !!").append("</h1>").toString();
+		String responseMsg = new StringBuilder("<h1>").append("Hey...Hello World 2012").append("</h1>").toString();
 		return responseMsg;
 	}
 
 	@RequestMapping(value = { "/hello/{msg}" }, method = RequestMethod.GET)
 	public String sayHelloMsg(@PathVariable String msg) {
-		String responseMsg = "Hello World";
+		String responseMsg = "Hello World 2021";
 		try {
 			responseMsg = new StringBuilder("<h1>").append("Hello ").append(msg).append("</h1>").toString();
 		} catch (Exception e) {
